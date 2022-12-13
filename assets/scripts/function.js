53b60df23cb88d271db8dd9f5d01be62876f390d
NNVG <g696607013@gmail.com>
2022-12-13 16:40:55 +0100

    Solve linter errors

--- assets/scripts/function.js
+++ assets/scripts/function.js
@@ -4,10 +4,8 @@ const btn1 = document.querySelector('.btn1')
 const btn2 = document.querySelector('.btn2')
 const btn3 = document.querySelector('.btn')
 const para = document.querySelector('.p1')
-let arr1 = [],
-  arr2 = [],
-  val,
-  val2
+let arr1 = [], arr2 = []
+let val, val2
 let result1, result2
 
 btn1.onclick = function input1 () {
@@ -31,13 +29,16 @@ btn2.onclick = function input2 () {
 }
 
 btn3.onclick = function scaleBalancing () {
-  let found = false, vall1, vall2
+  let found = false
+  let vall1, vall2
   for (let i = 0; i < arr2.length; i++) {
     for (let j = 0; j < arr2.length; j++) {
       result1 = parseInt(arr1[0]) + parseInt(arr2[i])
       result2 = parseInt(arr1[1]) + parseInt(arr2[j])
       if (result1 === result2) {
-        found = true, vall1 = arr2[j], vall2 = arr2[i]
+        found = true,
+        vall1 = arr2[j]
+        vall2 = arr2[i] 
         break
       }
     }
